a0645b01f817e21d29e67c7d3059d9e63546c361
RubenMartinG1 <rubenmh@hotmail.es>
2023-01-23 20:41:05 +0100

    Animaciones+ movimiento personaje
    ya estoy empezando a implementar las animaciones y moviemiento de personaje

--- Zombieland/Assets/Scripts/PlayerController.cs
+++ Zombieland/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,34 @@ public class PlayerController : MonoBehaviour
     float StrafeR;
     float StrafeL;
     float speed;
-    float WalkingSpeed = 1f;
-    float RunningSpeed = 2f;
+    float WalkingSpeed = 20f;
+    float RunningSpeed = 40f;
     float rotationSpeed;
-    float StrafeSpeed;
+    float StrafeSpeed = 20f;
+  
     InputActions inputActions;
     CharacterController cc;
-    Animator animator;
+    Animator anim;
     // Start is called before the first frame update
     private void Awake()
     {
-        animator = GetComponent<Animator>();
+        inputActions = new InputActions();
+        anim = GetComponent<Animator>();
         cc = GetComponent<CharacterController>();
+
+        inputActions.Player.Move.performed += ctx => move = ctx.ReadValue<float>();
+        inputActions.Player.Move.canceled += _ => move = 0f;
+
+        inputActions.Player.Run.performed += _ => anim.GetBool("Run");
+        inputActions.Player.Run.canceled += _ => anim.GetBool("Run");
+
+        inputActions.Player.StrafeL.performed += ctx => StrafeL = ctx.ReadValue<float>();
+        inputActions.Player.StrafeL.canceled += _ => StrafeL = 0f;
+
+        inputActions.Player.StrafeR.performed += ctx => StrafeR = ctx.ReadValue<float>();
+        inputActions.Player.StrafeR.canceled += _ => StrafeR = 0f;
+
+
     }
     void Start()
     {
@@ -30,6 +46,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         Walk();
         rotar();
         Run();
@@ -37,17 +54,20 @@ public class PlayerController : MonoBehaviour
     }
     void Walk()
     {
-        if (Animator.setfloat("Move") $$ move > 0.2f);
+        if (anim.GetBool("Run") && move > 0.2)
         {
+            print(move);
             speed = RunningSpeed;
         }
         else
         {
-            speed = move * WalkingSpeed;
+            speed = WalkingSpeed;
+            
         }
 
 
         cc.SimpleMove(transform.forward * move * speed);
+        anim.SetFloat("Move", move);
     }
     void rotar()
     {
@@ -60,7 +80,7 @@ public class PlayerController : MonoBehaviour
     void strafe()
     {
         float strafe = StrafeR - StrafeL;
-        Animator.SetFloat("Strafe", strafe);
+        anim.SetFloat("Strafe", strafe);
         cc.SimpleMove(transform.right * strafe * StrafeSpeed);
     }
     private void OnEnable()
