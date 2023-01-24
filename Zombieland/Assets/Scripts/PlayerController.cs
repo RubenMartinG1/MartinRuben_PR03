3ee088b2599ecf2061730a729023df6d91def2c8
RubenMartinG1 <rubenmh@hotmail.es>
2023-01-24 17:57:40 +0100

    moveset casi terminado
    ya he empezado a terminar el moveset del personaje

--- Zombieland/Assets/Scripts/PlayerController.cs
+++ Zombieland/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,17 @@ public class PlayerController : MonoBehaviour
     float StrafeR;
     float StrafeL;
     float speed;
-    float WalkingSpeed = 20f;
+    float WalkingSpeed = 2f;
     float RunningSpeed = 40f;
-    float rotationSpeed;
+    float rotationSpeed = 1f;
     float StrafeSpeed = 20f;
+    float running;
+    float strafe;
   
+
+
+
+
     InputActions inputActions;
     CharacterController cc;
     Animator anim;
@@ -27,14 +33,18 @@ public class PlayerController : MonoBehaviour
         inputActions.Player.Move.performed += ctx => move = ctx.ReadValue<float>();
         inputActions.Player.Move.canceled += _ => move = 0f;
 
-        inputActions.Player.Run.performed += _ => anim.GetBool("Run");
-        inputActions.Player.Run.canceled += _ => anim.GetBool("Run");
+        inputActions.Player.Run.performed += ctx => running = ctx.ReadValue<float>();
+        inputActions.Player.Run.canceled += _ => running = 0f;
 
         inputActions.Player.StrafeL.performed += ctx => StrafeL = ctx.ReadValue<float>();
         inputActions.Player.StrafeL.canceled += _ => StrafeL = 0f;
 
         inputActions.Player.StrafeR.performed += ctx => StrafeR = ctx.ReadValue<float>();
         inputActions.Player.StrafeR.canceled += _ => StrafeR = 0f;
+
+        inputActions.Player.Rotate.performed += ctx => rotate = ctx.ReadValue<float>();
+        inputActions.Player.Rotate.canceled += _ => rotate = 0f;
+
 
 
     }
@@ -44,19 +54,12 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-       
-        Walk();
-        rotar();
-        Run();
-        strafe();
-    }
+ 
     void Walk()
     {
         if (anim.GetBool("Run") && move > 0.2)
         {
-            print(move);
+          
             speed = RunningSpeed;
         }
         else
@@ -64,24 +67,53 @@ public class PlayerController : MonoBehaviour
             speed = WalkingSpeed;
             
         }
-
-
-        cc.SimpleMove(transform.forward * move * speed);
-        anim.SetFloat("Move", move);
+    cc.SimpleMove(transform.forward * move * speed);
+    anim.SetFloat("Move", move);
+ 
     }
     void rotar()
     {
-    //    Transform.rotate(Vector3.up * rotate * rotationSpeed);
+        //Transform.Rotate(Vector3.up * rotate * rotationSpeed);
     }
     void Run()
     {
-
-    }
-    void strafe()
+    if (running > 0.1)
     {
-        float strafe = StrafeR - StrafeL;
+         
+        anim.SetBool("Run", true);
+        speed = RunningSpeed;
+    }
+    else
+    {
+        
+        anim.SetBool("Run", false);
+        speed = WalkingSpeed;
+    }
+    cc.SimpleMove(transform.forward * move * speed);
+}
+    void Strafe()
+    {
+        strafe = StrafeR - StrafeL;
         anim.SetFloat("Strafe", strafe);
         cc.SimpleMove(transform.right * strafe * StrafeSpeed);
+    }
+
+    void Update()
+    {
+        
+
+        if (strafe == 0)
+        {
+            print("hola");
+            Walk();
+        }
+        if (strafe != 1)
+        {
+            
+            Strafe();
+        }
+        rotar();
+        Run();
     }
     private void OnEnable()
     {
